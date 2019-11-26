923c96358b1451c2990dfad13b8093f0938f7b4a
Paulves92 <paulanders92@gmail.com>
2019-11-26 16:10:45 +0100

    doing something with vue

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -3,7 +3,7 @@
     <form name = "login">
     <div class ="login">
         <input v-if="username.name === 'username'" type ="text" placeholder ="username" name = userid><br>
-        <input type ="password" placeholder="password" name=pswrd><br>
+        <input v-else="login.password === 'password'" type ="password" placeholder="password" name=pswrd><br>
          <input type="button" onclick="check(this.form)" value="Login"/>
          <router-link to="/" id="loginBtn" class="smallBtn">Guest</router-link>
         </div>
