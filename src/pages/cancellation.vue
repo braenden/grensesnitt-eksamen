f49918ae33a57c1df37a64ca8a835f721308f412
kevin <kebra98@gmail.com>
2019-11-26 16:03:19 +0100

    Progress on cancellation, login and yourReservations

--- src/pages/cancellation.vue
+++ src/pages/cancellation.vue
@@ -1,3 +1,29 @@
 <template>
-    
+    <div>
+        <input v-model="cancellationForm.firstName" type="text" placeholder="Fornavn"><br>
+        <input v-model="cancellationForm.surname" type="text" placeholder="Etternavn"><br>
+        <input v-model="cancellationForm.email" type="text" placeholder="Email addresse"><br>
+        <textarea v-model="cancellationForm.message" type="text" placeholder="Din melding"></textarea><br>
+        <button type="button" @click="sendMessage">Avbestill time</button>
+    </div>
 </template>
+<script>
+export default {
+    name: "cancellation",
+    data () {
+        return {
+            cancellationForm: [
+                {firstName: ""},
+                {surName: ""},
+                {email: ""},
+                {message: ""}
+            ]
+        }
+    },
+    methods: {
+        sendMessage () {
+            alert("Melding sent fra ")
+        }
+    }
+}
+</script>
---
+++ src/pages/login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>      
+    <form name = "login">
+    <div class ="login">
+        <input v-if="username.name === 'username'" type ="text" placeholder ="username" name = userid><br>
+        <input type ="password" placeholder="password" name=pswrd><br>
+         <input type="button" onclick="check(this.form)" value="Login"/>
+         <router-link to="/" id="loginBtn" class="smallBtn">Guest</router-link>
+        </div>
+    </form>
+    </div>
+</template>
+<script> 
+    export default {
+        name: "login",
+        data () {
+            return {
+                login: {
+                    username: "",
+                    password: ""
+                }
+            }
+        },
+        methods: {
+        }
+    }
+</script>
