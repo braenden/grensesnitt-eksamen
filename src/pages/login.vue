eaa5e5a6f9e55f6c6d9b8fed5c299c4ae91c8d46
kevin <kebra98@gmail.com>
2019-11-27 16:24:28 +0100

    Hva skjer nå

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,47 +1,15 @@
 <template>
-  <div>
     <div id="loginContainer">
-      <div id="loginBox">
-        <br />
-        <br />
-        <br />
-        <input
-          type="text"
-          name="username"
-          v-model="input.username"
-          placeholder="Username"
-          class="inputStyle"
-        />
-        <br />
-        <input
-          type="password"
-          name="password"
-          v-model="input.password"
-          placeholder="Password"
-          class="inputStyle"
-        />
-        <br />
-        <button id="loginButton" to="/staff" type="button" v-on:click="login()">Login</button>
-        <br />
-        <br />
-        <img id="johannes" src="../assets/johannesBrodwall.jpeg" />
-        <!-- Bilde av mannen selv -->
-        <br />
-        <br />
+        <div id="loginBox"><br><br><br>
+        <input type="text" name="username" v-model="input.username" placeholder="Username" class="inputStyle"/><br>
+        <input type="password" name="password" v-model="input.password" placeholder="Password" class="inputStyle"/><br>
+        <button id="loginButton" to="/staff" type="button" v-on:click="login()">Login</button><br><br>
+        <img id="johannes" src="../assets/johannesBrodwall.jpeg"> <!-- Bilde av mannen selv -->
+        <br><br>
         <router-link to="/" id="smallBtn8">Tilbake til forsiden</router-link>
-        <br />
-        <br />
-        <br />
-      </div>
+        <br><br><br>
+        </div>
     </div>
-    <br />
-    <br />
-    <br />
-    <router-link to="/" id="smallBtn8">Tilbake til forsiden</router-link>
-    <br />
-    <br />
-    <br />
-  </div>
 </template>
 
 <script>
@@ -60,7 +28,7 @@ export default {
       if (this.input.username == "admin" && this.input.password == "password") {
         this.$router.replace({ name: "staff" });
       } else {
-        alert("The username and / or password is incorrect");
+        alert("The username or password is incorrect");
       }
     }
   }
@@ -68,48 +36,47 @@ export default {
 </script>
 
 <style>
-#loginBox {
-  background-color: rgb(120, 120, 120);
-  display: inline-block;
-  width: 360px;
-  height: 525px;
-  margin-top: 20px;
-  border-radius: 5px 5px 5px 5px;
-  box-shadow: 0px 0px 1px 0px black;
+
+#johannes {
+    height: 200px;
+    width: 200px;
 }
 
 .inputStyle {
-  width: 300px;
-  height: 30px;
-  margin-top: 10px;
+    width: 250px;
+    height: 30px;
+    margin-top: 10px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-weight: bold;
+    box-shadow: 0px 0px 1px 0px black;
 }
 
 #loginButton {
-  text-decoration: none;
-  display: inline-block;
-  text-align: center;
-  font-size: 20px;
-  width: 120px;
-  height: 35px;
-  border-radius: 5px 5px 5px 5px;
-  background-color: purple;
-  color: white;
-  font-weight: bold;
-  padding-top: 10px;
-  margin-top: 30px;
+    text-decoration: none;
+    display: inline-block;
+    text-align: center;
+    font-size: 20px;
+    width: 120px;
+    height: 35px;
+    border-radius: 5px 5px 5px 5px;
+    background-color: darkgoldenrod;
+    color: white;
+    font-weight: bold;
+    padding-top: 10px;
+    margin-top: 30px;
 }
 
 #smallBtn8 {
-  background-color: purple;
-  color: white;
-  font-size: 15px;
-  text-decoration: none;
-  display: inline-block;
-  height: 30px;
-  width: 150px;
-  padding-top: 10px;
-  font-weight: bold;
-  border: none;
-  border-radius: 5px 5px 5px 5px;
+    background-color: purple;
+    color: white;
+    font-size: 15px;
+    text-decoration: none;
+    display: inline-block;
+    height: 30px;
+    width: 150px;
+    padding-top: 10px;
+    font-weight: bold;
+    border: none;
+    border-radius: 5px 5px 5px 5px;  
 }
 </style>
